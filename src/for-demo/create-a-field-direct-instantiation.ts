740130991c4e1c1b802b585bcc8b4811acb16144
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2024-04-04 23:28:48 -0700

    builders

--- src/for-demo/create-a-field-direct-instantiation.ts
+++ src/for-demo/create-a-field-direct-instantiation.ts
@@ -1,4 +1,14 @@
-import { CedarArtifactId, CedarBuilders, LinkField, LinkFieldBuilder, TextField, TextFieldBuilder } from 'cedar-model-typescript-library';
+import {
+  CedarArtifactId,
+  CedarBuilders,
+  LinkField,
+  LinkFieldBuilder,
+  TemporalFieldBuilder,
+  TextArea,
+  TextAreaBuilder,
+  TextField,
+  TextFieldBuilder,
+} from 'cedar-model-typescript-library';
 
 // const linkField0: LinkField = new LinkField();
 const linkField1: LinkField = LinkField.buildEmptyWithDefaultValues();
@@ -6,9 +16,20 @@ const linkField2: LinkField = LinkField.buildEmptyWithNullValues();
 const linkField3: LinkField = CedarBuilders.linkFieldBuilder().build();
 const linkField4: LinkField = new LinkFieldBuilder().build();
 
-const textField3: TextField = CedarBuilders.textFieldBuilder().build();
+const textField1: TextField = CedarBuilders.textFieldBuilder().build();
 
 const textFieldBuilder: TextFieldBuilder = CedarBuilders.textFieldBuilder();
 textFieldBuilder.withTitle('foo');
-const textField: TextField = textFieldBuilder.build();
-textField.at_id = CedarArtifactId.forValue('foo');
+const textField2: TextField = textFieldBuilder.build();
+textField2.at_id = CedarArtifactId.forValue('foo');
+
+const textArea0: TextArea = CedarBuilders.textAreaBuilder().build();
+
+const textAreaBuilder1 = CedarBuilders.textAreaBuilder();
+textAreaBuilder1.withTitle('foo');
+const textArea1: TextArea = textAreaBuilder1.build();
+textArea1.at_id = CedarArtifactId.forValue('foo');
+
+const builder1: TextFieldBuilder = CedarBuilders.textFieldBuilder();
+const builder2: TextAreaBuilder = CedarBuilders.textAreaBuilder();
+const builder3: TemporalFieldBuilder = CedarBuilders.temporalFieldBuilder();
